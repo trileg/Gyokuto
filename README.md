098c98d4d8e7d4345fe9b3f1130a8569160af26e
Kensuke Kosaka <k@trileg.net>
2015-12-31 18:08:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# 玉兎 Gyokuto
+[@tri_leg](https://twitter.com/tri_leg)の生活を支えてくれる，相棒．
+[@gyokutmo](https://twitter.com/gyokutmo)で運用する予定．  
+
+## Requirement
+- Python 3.x
+- [Python Twitter Tools](https://github.com/sixohsix/twitter "https://github.com/sixohsix/twitter")
+  - `pip install twitter`
+
+## Author
+[Kensuke Kosaka](https://github.com/trileg "https://github.com/trileg")
---
+++ brain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from twitter import *
+
+
+def brain():
+    api_key = os.environ.get("GYOKUTO_API_KEY")
+    api_secret = os.environ.get("GYOKUTO_API_SECRET")
+    access_token = os.environ.get("GYOKUTO_ACCESS_TOKEN")
+    access_token_secret = os.environ.get("GYOKUTO_ACCESS_TOKEN_SECRET")
+    dm_to = os.environ.get("GYOKUTO_DM_TO")
+
+    while True:
+        api = OAuth(access_token, access_token_secret, api_key, api_secret)
+        t = Twitter(auth=api)
+        twitter_userstream = TwitterStream(auth=api, domain='userstream.twitter.com')
+
+        for msg in twitter_userstream.user():
+            if 'direct_message' in msg:
+                message = msg['direct_message']['text']
+                if 'ping' in message:
+                    t.direct_messages.new(user=dm_to, text="pong")
+
+
+if __name__ == '__main__':
+    brain()
